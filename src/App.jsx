f57d733104acc8da4e163c7b6e7d79ff1958d6ae
haeji1124 <46364329+haeji1124@users.noreply.github.com>
2025-11-06 01:36:58 +0900

    구글 애널리틱스 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Test from "./pages/Test";
 import TestResult from "./pages/TestResult";
+import { useEffect } from "react";
+import ReactGA4 from "react-ga4";
 
-export const base_url = "http://localhost:5173";
+export const base_url = "https://www.haeji1124.shop";
 
 function App() {
+  useEffect(() => {
+    ReactGA4.initialize([
+      {
+        trackingId: "G-P9TKRVBTTW",
+        gaOptions: {
+          siteSpeedSampleRate: 100,
+        },
+      },
+    ]);
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
