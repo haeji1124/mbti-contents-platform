b94c04c8299c06cb0fbcf98a5c8cbc895e6b8b8f
haeji1124 <haeji1124@naver.com>
2025-11-18 13:14:54 +0900

    푸터추가

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <div>
+            <h5>광고 및 후원 문의 <br/>Advertising and Sponsorshop Contact</h5>
+            <p>haeji1124@naver.com</p>
+            <p>
+                MBTI_PLATFORM SHOP | 대표 신해지
+                <br />
+                주소: ~~~
+                <br />
+                호스팅 서비스 : CloudFlare
+                <br />
+                유선 번호: ~~~~~
+                <br />
+                신고 번호: ~~~~~
+                <p>
+                    
+                </p>
+            </p>
+        </div>
+    );
+}
+
+export default Footer;
